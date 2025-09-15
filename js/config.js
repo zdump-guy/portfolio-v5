232034a857bd4a554ee916296a3c26adb90f43d6
Ghost <mohamedmostafa.dev.main@gmail.com>
2025-09-16 01:51:34 +0300

    I am cooked

--- js/config.js
+++
@@ -1,2 +0,0 @@
-export const SUPABASE_URL = 'https://yimybtavfvvntcyjizdw.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpbXlidGF2ZnZ2bnRjeWppemR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4OTQ5NTQsImV4cCI6MjA3MzQ3MDk1NH0.VO3jDTbIZPTsjXfSU7HVd7FWU51WyGor2eS0Hnbv1Vo';
