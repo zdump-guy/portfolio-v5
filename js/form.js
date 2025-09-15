6bcf037100e506e0bdf0487ffcd0c0781a44a6cb
Ghost <mohamedmostafa.dev.main@gmail.com>
2025-09-16 01:53:01 +0300

    f this form

--- js/form.js
+++ js/form.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '/portfolio-v5/js/config.js';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from './js/config.js';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
